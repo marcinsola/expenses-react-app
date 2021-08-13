9ae0b48635c37c9c46d346aac82501bb623119da
Marcin Sola <marcinsolaonline@gmail.com>
2021-08-13 15:36:55 +0200

    Use arrow notation for components

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -2,7 +2,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './ExpenseList.css';
 
-function ExpenseList(props) {
+const ExpenseList = (props) => {
     const renderedExpenses = props.expenses.map(
         expense => <ExpenseItem date={expense.date}
             amount={expense.amount}
