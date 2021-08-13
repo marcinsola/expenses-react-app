eadff397f4b23da2b2154da9a534dcb503ec7146
Marcin Sola <marcinsolaonline@gmail.com>
2021-08-13 15:24:32 +0200

    After first sum-up

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,19 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import './ExpenseList.css';
+
+function ExpenseList(props) {
+    const renderedExpenses = props.expenses.map(
+        expense => <ExpenseItem date={expense.date}
+            amount={expense.amount}
+            title={expense.title} />
+    );
+
+    return (
+        <Card className="expenses">
+            {renderedExpenses}
+        </Card>
+    )
+}
+
+export default ExpenseList;
